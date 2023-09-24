4565fc4c524316e4a158e88afc5004d64cffdad3
mangosax <mcguerty@att.net>
2023-09-24 17:17:02 -0500

    Dapper Exercise 1 working

---
+++ ORM-Dapper/DapperProductRepository.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+
+    
+
+
+    public class DapperProductRepository : IProductRepository
+    {
+
+        private readonly IDbConnection _conn;
+
+        public DapperProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM prodcuts;");
+        }
+
+
+       
+
+
+    }//class
+}//namespace
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace ORM_Dapper
+﻿using System;
+using System.Data;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using ORM_Dapper;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+            var config = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json")
+                            .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+
+            IDbConnection conn = new MySqlConnection(connString);
+
+            var departmentRepo = new DapperDepartmentRepository(conn);
+
+
+            var departments = departmentRepo.GetAllDepartments();
+
+            foreach (var deparatment in departments)
+            {
+                Console.WriteLine(deparatment.DepartmentID);
+                Console.WriteLine(deparatment.Name);
+                Console.WriteLine("\n\n");
+
+            }
+
+        }//Main Method
+
+
+    }//Class
+}//namespace ORM_Dapper
